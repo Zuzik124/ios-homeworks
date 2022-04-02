5c8ba97e238d2c649ae0e9367d47aa26987c7337
Zuzik124 <pyot-makaro@yandex.ru>
2022-04-02 16:23:05 +0300

    Initial Commit

---
+++ Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Петр Макаров on 02.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
